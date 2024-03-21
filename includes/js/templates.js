520fcbf46d1f8e7aeedd5c73d1d7f97c80cbb45f
Sara Cook <saracook@stanford.edu>
2024-03-21 12:48:26 -0700

    troubleshooting github deployment

--- includes/js/templates.js
+++ includes/js/templates.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-	console.log('templates.js');
   $('#identityBar').load('./includes/html/identitybar.html');
-  $('footer').load('/includes/html/footer.html');
-$('#masthead').load('/includes/html/masthead.html');
-$('#skip-link').load('/includes/html/skiplink.html');
+  $('footer').load('./includes/html/footer.html');
+  $('#masthead').load('./includes/html/masthead.html');
+  $('#skip-link').load('./includes/html/skiplink.html');
 });
